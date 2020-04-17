55eb6f8dc765a2b4304f9dc58b1723a0a0b4f660
Jayant Rai <jayant.chamling@outlook.com>
2020-04-17 17:23:41 -0500

    redux data store and navigation

--- src/Shop/CategoryNavigation.js
+++ src/Shop/CategoryNavigation.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { ToggleLink } from '../ToggleLink'
 
 export class CategoryNavigation extends Component {
   render () {
-    return <>
-      <Link
+    return <React.Fragment>
+      {/* <Link
         className='btn btn-secondary btn-block'
         to={this.props.baseUrl}
       >All
-      </Link>
+      </Link> */}
+      <ToggleLink to={ this.props.baseUrl } exact={true}>All</ToggleLink>
       {this.props.categories && this.props.categories.map(cat =>
-        <Link
-          className='btn btn-secondary btn-block' key={cat}
-          to={`${this.props.baseUrl}/${cat.toLowerCase()}`}
-        >
+        <ToggleLink key={ cat }
+            // <Link
+        //   className='btn btn-secondary btn-block' key={cat}
+          to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
           {cat}
-        </Link>
+        {/* </Link> */}
+        </ToggleLink>
       )}
-           </>
+           </React.Fragment>
   }
 }
 
